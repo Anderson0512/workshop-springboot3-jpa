daede8cb393deed06dd89be5437bc2668ec0c82c
Anderson0512 <joao_anderson26@hotmail.com>
2022-12-13 18:12:56 -0300

    Service Layer, component registration

---
+++ src/main/java/com/smarthome/course/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package com.smarthome.course.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public static final long serialVersionUID = 1L;
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/smarthome/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.smarthome.course.services;
+
+import com.smarthome.course.entities.User;
+import com.smarthome.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+}
