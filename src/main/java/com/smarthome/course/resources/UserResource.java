e1fe12244221555e82e49b80eef59658b71a11c3
Anderson0512 <joao_anderson26@hotmail.com>
2022-12-13 19:42:44 -0300

    Implementation methods insert, update and delete user

--- src/main/java/com/smarthome/course/resources/UserResource.java
+++ src/main/java/com/smarthome/course/resources/UserResource.java
@@ -3,11 +3,9 @@ package com.smarthome.course.resources;
 import com.smarthome.course.entities.User;
 import com.smarthome.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,26 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        userService.insert(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public User update(@RequestBody User user, @PathVariable Long id) {
+        userService.update(user,id);
+        return user;
+    }
+
+    @PostMapping("/remove/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String delete(@PathVariable Long id) {
+        userService.delete(id);
+        return "Success. User deleted!!!";
     }
 }
--- src/main/java/com/smarthome/course/services/UserService.java
+++ src/main/java/com/smarthome/course/services/UserService.java
@@ -1,11 +1,13 @@
 package com.smarthome.course.services;
 
 import com.smarthome.course.entities.User;
+import com.smarthome.course.exception.BusinessException;
 import com.smarthome.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
@@ -23,5 +25,29 @@ public class UserService {
 
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public void insert(User user) {
+        userRepository.save(user);
+    }
+
+    public void update(User user, Long id) {
+        User entityBd = this.findById(id);
+
+        if (entityBd.getId().equals(user.getId())){
+            userRepository.save(user);
+        }else {
+            throw new BusinessException("Identifiers for change are divergent!");
+        }
+    }
+
+    public void delete(Long id) {
+        User entityBd = this.findById(id);
+
+        if (Objects.nonNull(entityBd.getId())){
+            userRepository.delete(entityBd);
+        }else {
+            throw new BusinessException("Identifiers of Identity not found!");
+        }
     }
 }
