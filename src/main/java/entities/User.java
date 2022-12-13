4caf24630d94ca104fd4887d67d88c8acd17728d
Anderson0512 <joao_anderson26@hotmail.com>
2022-12-13 17:08:25 -0300

    H2 database, test profile, JPA

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,27 +1,30 @@
 package entities;
 
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @ToString
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-    @Serial
     private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public User(){}
+    public User() {
+    }
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
@@ -29,5 +32,18 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
