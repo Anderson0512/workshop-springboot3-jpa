2bdbc5067c9a46170fc1848f1278ad1607ac005b
Anderson0512 <joao_anderson26@hotmail.com>
2022-12-14 11:16:09 -0300

    Product

---
+++ src/main/java/com/smarthome/course/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.smarthome.course.services;
+
+import com.smarthome.course.entities.Product;
+import com.smarthome.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+}
