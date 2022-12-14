b011ac9eee97d6823ee6c5296d29ecaf819ba209
Anderson0512 <joao_anderson26@hotmail.com>
2022-12-13 22:04:20 -0300

    OrderStatus Enum

--- src/main/java/com/smarthome/course/entities/Order.java
+++ src/main/java/com/smarthome/course/entities/Order.java
@@ -1,7 +1,9 @@
 package com.smarthome.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.smarthome.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,6 +23,10 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,9 +34,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -45,5 +52,15 @@ public class Order implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (Objects.nonNull(orderStatus)){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 }
